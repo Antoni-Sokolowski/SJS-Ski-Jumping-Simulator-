8991999d4ef204e2ad2251e78b974463c29f9c73
Antoni Sokołowski <antoni.as.sokolowski@outlook.com>
2025-08-25 00:22:56 +0200

    Fix import error by adding ui/__init__.py and other improvements

---
+++ ui/__init__.py
@@ -0,0 +1,9 @@
+from .animations import AnimatedStackedWidget
+from .components import NavigationSidebar, ModernComboBox, ModernSlider
+
+__all__ = [
+    "AnimatedStackedWidget",
+    "NavigationSidebar",
+    "ModernComboBox",
+    "ModernSlider",
+]
